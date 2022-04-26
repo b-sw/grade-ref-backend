2966dc48551d6da3780932c97b49169dbae95df9
Bartosz Świtalski <bartoszswitalski@icloud.com>
2022-04-26 22:39:34 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     .setVersion('0.1')
     .addBearerAuth()
     .build();
+  app.enableCors({ origin: '*' });
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
