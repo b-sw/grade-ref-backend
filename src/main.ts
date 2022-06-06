56ebfb4e21b8033d2030805c751571bea913ccaf
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2022-06-06 23:26:39 +0200

    feat: add AWS secret manager

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const dotenv = require("dotenv");
+const fs = require("fs").promises;
+const retrieveSecrets = require("./retrieveSecrets");
 
 async function bootstrap() {
+  const secretsString = await retrieveSecrets();
+  await fs.writeFile(".env", secretsString);
+  console.log(secretsString);
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Grade-ref-API')
---
+++ src/retrieveSecrets.ts
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+
+module.exports = () => {
+    //configure AWS SDK
+    const region = "eu-central-1";
+    const client = new AWS.SecretsManager({ region });
+
+    const SecretId = "ersms-secrets-nobase64";
+    return new Promise((resolve, reject) => {
+        //retrieving secrets from secrets manager
+        client.getSecretValue({ SecretId }, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const secretsJSON = JSON.parse(data.SecretString);
+                let secretsString = "";
+                Object.keys(secretsJSON).forEach((key) => {
+                    secretsString += `${key}=${secretsJSON[key]}\n`;
+                });
+                resolve(secretsString);
+            }
+        });
+    });
+};
