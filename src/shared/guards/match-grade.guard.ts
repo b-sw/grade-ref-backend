9e36950e813b03798854e52a690e1a654024d038
Bartosz Świtalski <bartoszswitalski@icloud.com>
2022-06-13 10:46:33 +0200

    Adjust sms receiving

--- src/shared/guards/match-grade.guard.ts
+++ src/shared/guards/match-grade.guard.ts
@@ -1,30 +1,26 @@
-import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { MatchesService } from '../../matches/matches.service';
-import { UsersService } from '../../users/users.service';
-import { User } from '../../entities/user.entity';
-import { Match } from '../../entities/match.entity';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { UpdateGradeSmsDto } from '../../matches/dto/update-grade-sms.dto';
 
 @Injectable()
 export class MatchGradeGuard implements CanActivate {
-  constructor(private matchesService: MatchesService,
-              private usersService: UsersService) {}
+  constructor() {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const dto: UpdateGradeSmsDto = request.body;
+    console.log('request in guard is', request);
     console.log('request body in guard is', dto);
 
-    if (!dto.message || !dto.message.msisdn || !dto.message.id) {
-      throw new HttpException('Invalid body.', HttpStatus.BAD_REQUEST);
-    }
-
-    const observer: User | undefined = await this.usersService.getByPhoneNumber(dto.message.msisdn);
-    const match: Match | undefined = await this.matchesService.getByObserverSmsId(dto.message.id);
-
-    if (!observer || !match || observer.id !== match.observerId) {
-      throw new HttpException('Forbidden.', HttpStatus.FORBIDDEN);
-    }
+    // if (!dto.message || !dto.message.msisdn || !dto.message.id) {
+    //   throw new HttpException('Invalid body.', HttpStatus.BAD_REQUEST);
+    // }
+    //
+    // const observer: User | undefined = await this.usersService.getByPhoneNumber(dto.message.msisdn);
+    // const match: Match | undefined = await this.matchesService.getByObserverSmsId(dto.message.id);
+    //
+    // if (!observer || !match || observer.id !== match.observerId) {
+    //   throw new HttpException('Forbidden.', HttpStatus.FORBIDDEN);
+    // }
     return true;
   }
 }
