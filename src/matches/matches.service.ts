6245665e1bd8162f0f6030a65bf2525ad85970b6
Bartosz Świtalski <bartoszswitalski@icloud.com>
2022-06-15 13:05:33 +0200

    Adjust sms grade receiving

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -127,6 +127,7 @@ export class MatchesService {
 
   async updateGradeSms(gradeMessage: GradeMessage, observer: User): Promise<void> {
     if (!(await this.requireSmsValid(gradeMessage.msg, observer.phoneNumber))) {
+      console.log('invalid sms');
       return;
     }
 
@@ -134,10 +135,12 @@ export class MatchesService {
     const match: Match | undefined = await this.getByUserReadableKey(matchKey);
 
     if (!(await this.requireMatchValid(match, observer.phoneNumber))) {
+      console.log('invalid match');
       return;
     }
 
     if (!(await this.requireGradeValid(gradeMessage.msg, observer.phoneNumber))) {
+      console.log('invalid grade');
       return;
     }
 
@@ -145,6 +148,7 @@ export class MatchesService {
     match.refereeGradeDate = new Date();
     await this.matchRepository.save(match);
     await this.sendOneWaySms(observer.phoneNumber, `Grade for match ${match.userReadableKey} has been entered.`);
+    console.log('sent sms');
   }
 
   async requireSmsValid(smsText: string, phoneNumber: string): Promise<boolean> {
