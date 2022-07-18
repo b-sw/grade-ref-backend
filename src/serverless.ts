253593b550fef3f320e8597f2789361e41e605ca
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-07-18 20:11:07 +0200

    Add binary media types to sls bootstrap

--- src/serverless.ts
+++ src/serverless.ts
@@ -8,33 +8,30 @@ let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Grade-ref-API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  app.enableCors({ origin: '*' });
+  const config = new DocumentBuilder().setTitle('Grade-ref-API').setVersion('1.0').addBearerAuth().build();
+  app.enableCors({
+    origin: '*',
+    allowedHeaders:
+      'Content-Type, Access-Control-Allow-Headers, Access-Control-Expose-Headers, Content-Disposition, Authorization, X-Requested-With',
+    exposedHeaders: 'Content-Disposition',
+  });
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  return serverlessExpress({ app: expressApp, binaryMimeTypes: ['application/pdf'] });
 }
 
-export const handler: Handler = async (
-  event: any,
-  context : Context,
-  callback : Callback,
-) => {
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
   if (event.source === 'serverless-plugin-warmup') {
     console.log('WarmUp - Lambda is warm!');
     /** Slightly delayed (25ms) response
      to ensure concurrent invocation */
-    await new Promise(r => setTimeout(r, 25));
+    await new Promise((r) => setTimeout(r, 25));
     return 'Lambda is warm!';
   }
 
   server = server ?? (await bootstrap());
   return server(event, context, callback);
-}
+};
